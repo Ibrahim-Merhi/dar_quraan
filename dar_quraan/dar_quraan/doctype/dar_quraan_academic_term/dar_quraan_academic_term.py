2e1b1ad3e929a197fbc0dc512d4f28e33ed6a67c
Ibrahim-Merhi <ibrahim.m.merhy@gmail.com>
2026-07-26 14:15:30 +0000

    feat: add Dar Quraan Academic Term doctype

---
+++ dar_quraan/dar_quraan/doctype/dar_quraan_academic_term/dar_quraan_academic_term.py
@@ -0,0 +1,129 @@
+import frappe
+from frappe import _
+from frappe.model.document import Document
+from frappe.utils import getdate, formatdate
+
+
+class DarQuraanAcademicTerm(Document):
+    def validate(self):
+        self.normalize_values()
+        self.validate_dates()
+        self.validate_academic_year()
+        self.validate_term_within_academic_year()
+        self.validate_unique_term()
+        self.validate_current_term()
+        self.apply_status_rules()
+
+    def normalize_values(self):
+        if self.term_name:
+            self.term_name = self.term_name.strip()
+
+    def validate_dates(self):
+        if not self.start_date or not self.end_date:
+            return
+
+        if getdate(self.end_date) <= getdate(self.start_date):
+            frappe.throw(_("End Date must be later than Start Date."))
+
+    def validate_academic_year(self):
+        if not self.academic_year:
+            return
+
+        academic_year = frappe.db.get_value(
+            "Dar Quraan Academic Year",
+            self.academic_year,
+            ["disabled", "status"],
+            as_dict=True,
+        )
+
+        if not academic_year:
+            frappe.throw(_("The selected Academic Year does not exist."))
+
+        if academic_year.disabled:
+            frappe.throw(_("A term cannot be linked to a disabled Academic Year."))
+
+        if academic_year.status == "Archived":
+            frappe.throw(_("A term cannot be linked to an archived Academic Year."))
+
+    def validate_term_within_academic_year(self):
+        if not self.academic_year or not self.start_date or not self.end_date:
+            return
+
+        academic_year = frappe.db.get_value(
+            "Dar Quraan Academic Year",
+            self.academic_year,
+            ["start_date", "end_date"],
+            as_dict=True,
+        )
+
+        if not academic_year:
+            return
+
+        term_start = getdate(self.start_date)
+        term_end = getdate(self.end_date)
+        year_start = getdate(academic_year.start_date)
+        year_end = getdate(academic_year.end_date)
+
+        if term_start < year_start or term_end > year_end:
+            frappe.throw(
+    _(
+        "Term dates must fall between {0} and {1}, "
+        "the dates of Academic Year {2}."
+    ).format(
+        formatdate(year_start),
+        formatdate(year_end),
+        frappe.bold(self.academic_year),
+    )
+)
+
+    def validate_unique_term(self):
+        if not self.term_name or not self.academic_year:
+            return
+
+        existing_term = frappe.db.exists(
+            "Dar Quraan Academic Term",
+            {
+                "term_name": self.term_name,
+                "academic_year": self.academic_year,
+                "name": ["!=", self.name or ""],
+            },
+        )
+
+        if existing_term:
+            frappe.throw(
+                _("Term {0} already exists in Academic Year {1}.").format(
+                    frappe.bold(self.term_name),
+                    frappe.bold(self.academic_year),
+                )
+            )
+
+    def validate_current_term(self):
+        if not self.is_current:
+            return
+
+        if self.disabled:
+            frappe.throw(_("A disabled Academic Term cannot be current."))
+
+        existing_current = frappe.db.exists(
+            "Dar Quraan Academic Term",
+            {
+                "is_current": 1,
+                "name": ["!=", self.name or ""],
+            },
+        )
+
+        if existing_current:
+            frappe.throw(
+                _("Academic Term {0} is already marked as current.").format(
+                    frappe.bold(existing_current)
+                )
+            )
+
+    def apply_status_rules(self):
+        if self.status in ("Closed", "Archived"):
+            self.allow_new_enrollment = 0
+
+        if self.status == "Archived":
+            self.allow_attendance_entry = 0
+            self.allow_progress_entry = 0
+            self.is_current = 0
---
+++ dar_quraan/dar_quraan/doctype/dar_quraan_academic_term/test_dar_quraan_academic_term.py
@@ -0,0 +1,126 @@
+import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDarQuraanAcademicTerm(FrappeTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.academic_year = frappe.get_doc(
+            {
+                "doctype": "Dar Quraan Academic Year",
+                "naming_series": "DQAY-.#####",
+                "academic_year_name": "TEST TERM YEAR 2026-2027",
+                "start_date": "2026-09-01",
+                "end_date": "2027-08-31",
+                "status": "Open",
+                "allow_new_enrollment": 1,
+                "allow_attendance_entry": 1,
+                "allow_progress_entry": 1,
+            }
+        ).insert()
+
+    @classmethod
+    def tearDownClass(cls):
+        frappe.db.delete(
+            "Dar Quraan Academic Term",
+            {"academic_year": cls.academic_year.name},
+        )
+        frappe.db.delete(
+            "Dar Quraan Academic Year",
+            {"name": cls.academic_year.name},
+        )
+
+        super().tearDownClass()
+
+    def tearDown(self):
+        frappe.db.delete(
+            "Dar Quraan Academic Term",
+            {"academic_year": self.academic_year.name},
+        )
+
+    def make_term(self, **values):
+        data = {
+            "doctype": "Dar Quraan Academic Term",
+            "naming_series": "DQAT-.#####",
+            "term_name": "Test First Term",
+            "academic_year": self.academic_year.name,
+            "start_date": "2026-09-01",
+            "end_date": "2026-12-31",
+            "status": "Draft",
+            "allow_new_enrollment": 1,
+            "allow_attendance_entry": 1,
+            "allow_progress_entry": 1,
+        }
+
+        data.update(values)
+        return frappe.get_doc(data)
+
+    def test_term_dates_must_be_valid(self):
+        term = self.make_term(
+            start_date="2026-12-31",
+            end_date="2026-09-01",
+        )
+
+        self.assertRaises(frappe.ValidationError, term.insert)
+
+    def test_term_must_be_within_academic_year(self):
+        term = self.make_term(
+            start_date="2026-08-01",
+            end_date="2026-12-31",
+        )
+
+        self.assertRaises(frappe.ValidationError, term.insert)
+
+    def test_duplicate_term_name_in_same_year_is_rejected(self):
+        first_term = self.make_term()
+        first_term.insert()
+
+        duplicate_term = self.make_term()
+
+        self.assertRaises(frappe.ValidationError, duplicate_term.insert)
+
+    def test_only_one_current_term_is_allowed(self):
+        first_term = self.make_term(
+            term_name="Test First Current Term",
+            is_current=1,
+        )
+        first_term.insert()
+
+        second_term = self.make_term(
+            term_name="Test Second Current Term",
+            start_date="2027-01-01",
+            end_date="2027-04-30",
+            is_current=1,
+        )
+
+        self.assertRaises(frappe.ValidationError, second_term.insert)
+
+    def test_closed_term_disables_enrollment(self):
+        term = self.make_term(
+            status="Closed",
+            allow_new_enrollment=1,
+        )
+        term.insert()
+
+        self.assertEqual(term.allow_new_enrollment, 0)
+
+    def test_archived_term_disables_operations(self):
+        term = self.make_term(
+            status="Archived",
+            allow_new_enrollment=1,
+            allow_attendance_entry=1,
+            allow_progress_entry=1,
+        )
+        term.insert()
+
+        self.assertEqual(term.allow_new_enrollment, 0)
+        self.assertEqual(term.allow_attendance_entry, 0)
+        self.assertEqual(term.allow_progress_entry, 0)
+
+    def test_naming_series_generates_term_id(self):
+        term = self.make_term()
+        term.insert()
+
+        self.assertTrue(term.name.startswith("DQAT-"))
